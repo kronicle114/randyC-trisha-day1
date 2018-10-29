d5c2da13bb0f41772308f7606d93e91e4eb8683b
kronicle114 <trisha.aguinaldo@gmail.com>
2018-10-29 15:25:50 -0400

    updated drill1

--- drill1.js
+++ drill1.js
@@ -1,8 +1,8 @@
-
-createGreeting();
-
 function createGreeting (){
 
-    console.log("Hi, my name is Trisha and I'm 25 years old");
+    return "Hi, my name is Trisha and I'm 25 years old";
 
-}
+};
+
+const greeting1 = createGreeting();
+console.log(greeting1);
