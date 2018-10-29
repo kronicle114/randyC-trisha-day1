46a440571d60f0e9a403a5b2e202c07bcd39bcac
R-Cole <randy@goodglue.com>
2018-10-29 12:40:05 -0700

    changed js file again

--- drill1.js
+++ drill1.js
@@ -1,3 +1,12 @@
+
+function getYearOfBirth (age){
+    const yearOfBirth = 2018 - age;
+    return yearOfBirth;
+
+};
+ 
+
+
 function createGreeting (name,age){
     const yearOfBirth = 2018 - age;
     return `Hi, my name is ${name} and I'm 25 years old. I was born in ${yearOfBirth}.`;
