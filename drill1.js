4b18d1d52681b335be2ab03413866d06644a814f
kronicle114 <trisha.aguinaldo@gmail.com>
2018-10-29 16:38:54 -0400

    updated drill1.js

--- drill1.js
+++ drill1.js
@@ -1,45 +1,47 @@
+'use strict';
 
 function getYearOfBirth (age){
-    
-    if(typeof age === string || age === NaN || age === undefined){
+    console.log('year of birth');
+  // try{
+  if(typeof age === 'string' || isNaN(age) === true  || age === undefined){
+    console.log('hello world');
+    throw new Error('Age cannot be text. Please enter a number.');
+  }
+  if (age < 0){
+    throw new Error('Age can not be negative');
+  }
+  //  getYearOfBirth(25); recursion  a function that calls  itself
+  // const greeting1 = createGreeting('Trisha', 25);
+  // console.log(greeting1);
+  return 2018 - age;
+//   } catch(e) {
+//     console.log(e.message);
+//   }
+}
+ 
 
-         throw new Error("Age cannot be text. Please enter a number.");
+//     const yearOfBirth = 2018 - age;
+//     return yearOfBirth;
+//     try {
+//         getYearOfBirth(25);
+//         const greeting1 = createGreeting('Trisha', 25);
+//         console.log(greeting1);
+//     } catch(e) {
+//         console.error(e.message);
+//     }
+// };
 
-    }
-    try{
-        getYearOfBirth(25);
-        const greeting1 = createGreeting('Trisha', 25);
-        console.log(greeting1);
-    } catch(e) {
-
-        console.log(e.message);
-    }
-
-
-    }
-    
-    if (age < 0){
-        throw new Error("Age can not be negative");
-    }
-    const yearOfBirth = 2018 - age;
-    return yearOfBirth;
-    try {
-        getYearOfBirth(25);
-        const greeting1 = createGreeting('Trisha', 25);
-        console.log(greeting1);
-    } catch(e) {
-        console.error(e.message);
-    }
-};
-
-getYearOfBirth(-2);
 
 function createGreeting (name,age){
-    const yearOfBirth = 2018 - age;
+  try {
+    const yearOfBirth = getYearOfBirth(age);
     return `Hi, my name is ${name} and I'm 25 years old. I was born in ${yearOfBirth}.`;
+  } catch(e) {
+    console.error(e.message);
+  }
+}
 
-};
-
-const greeting1 = createGreeting('Trisha', 25);
+const greeting1 = createGreeting('Trisha', 20);
 console.log(greeting1);
+
 
